080f57fd9bccf387ba636bd03bc4e4efd1f6c9f5
odyssey <odissey.meltonyan@gmail.com>
2023-11-19 15:07:08 +0300

    Commit 1

---
+++ 1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def main():
+    school = {"1a": 20, "1б": 25, "2a": 18, "2б": 23, "3a": 21, "3б": 24, }
+    #изменение количество учащихся
+    school["1a"] += 1
+
+    #Добавление нового класса
+    school["4a"] = 20
+
+    #Удаление одного класса
+    del school["2a"]
+
+    count_of_students = sum(school.values())
+    return f"В школе с классами {school} - {count_of_students} учеников"
+
+if __name__ == "__main__":
+    print(main())
