92cb58afc2380d9cb5723ca1d8ddecad76f5e6ad
odyssey <odissey.meltonyan@gmail.com>
2023-12-27 00:18:13 +0300

    match/case fix

--- individual.py
+++ individual.py
@@ -33,22 +33,24 @@ def add_person(list_of_people):
     print("Человек добавлен\n")
 
 
-def find_person_by_phone(list_of_people, phone):
-    for person in list_of_people:
-        if person['номер телефона'] == phone:
-            return person
+def find_person_by_phone(people, phone):
+    for person in people:
+        match person['номер телефона']:
+            case phone:
+                return person
     return None
 
 
-def print_person(list_of_people):
-    if list_of_people:
-        print("\nИнформация о человеке:")
-        print(f"Фамилия: {list_of_people['фамилия']}")
-        print(f"Имя: {list_of_people['имя']}")
-        print(f"Номер телефона: {list_of_people['номер телефона']}")
-        print(f"Дата рождения: {list_of_people['дата рождения'].strftime('%d.%m.%Y')}\n")
-    else:
-        print("Человек не найден.\n")
+def print_person_info(list_of_people):
+    match list_of_people:
+        case []:
+            print("Человек не найден.\n")
+        case {'фамилия': f, 'имя': i, 'номер телефона': nt, 'дата рождения': dr}:
+            print("\nИнформация о человеке:")
+            print(f"Фамилия: {f}")
+            print(f"Имя: {i}")
+            print(f"Номер телефона: {nt}")
+            print(f"Дата рождения: {dr.strftime('%d.%m.%Y')}\n")
 
 
 def main():
@@ -61,19 +63,20 @@ def main():
         print("4. Выйти")
         choice = input("Выберите действие (1/2/3/4): ")
 
-        if choice == '1':
-            add_person(list_of_people)
-        elif choice == '2':
-            phone_to_find = input("Введите номер телефона для поиска: ")
-            found_person = find_person_by_phone(list_of_people, phone_to_find)
-            print_person(found_person)
-        elif choice == '3':
-            for _ in list_of_people: print_person(_)
-        elif choice == '4':
-            print("Программа завершена.")
-            break
-        else:
-            print("Некорректный ввод. Попробуйте снова.")
+        match choice:
+            case '1':
+                add_person(list_of_people)
+            case '2':
+                phone_to_find = input("Введите номер телефона для поиска: ")
+                found_person = find_person_by_phone(list_of_people, phone_to_find)
+                print_person_info(found_person)
+            case '3':
+                for _ in list_of_people: print_person_info(_)
+            case '4':
+                print("Программа завершена.")
+                break
+            case _:
+                print("Некорректный ввод. Попробуйте снова.")
 
 
 if __name__ == "__main__":
